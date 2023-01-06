222663d8bb513e837c70435028c02d5105df9093
tohda <tohda@chitoseka.com>
2023-01-06 13:28:33 +0900

    ブログページ作成

---
+++ libs/client.js
@@ -0,0 +1,6 @@
+import { createClient } from "microcms-js-sdk";
+
+export const client = createClient({
+  serviceDomain: "dqz1nji712",
+  apiKey: process.env.API_KEY,
+});
---
+++ pages/news/[id].js
@@ -0,0 +1,35 @@
+import { client } from "../../libs/client";
+
+export default function NewsId({ news }) {
+  return (
+    <main>
+      <h1>{news.title}</h1>
+      <p>{news.publishedAt}</p>
+      <div
+        dangerouslySetInnerHTML={{
+          __html: `${news.body}`,
+        }}
+      />
+    </main>
+  );
+}
+
+// 静的生成のためのパスを指定します
+export const getStaticPaths = async () => {
+  const data = await client.get({ endpoint: "news" });
+
+  const paths = data.contents.map((content) => `/news/${content.id}`);
+  return { paths, fallback: false };
+};
+
+// データをテンプレートに受け渡す部分の処理を記述します
+export const getStaticProps = async (context) => {
+  const id = context.params.id;
+  const data = await client.get({ endpoint: "news", contentId: id });
+
+  return {
+    props: {
+      news: data,
+    },
+  };
+};
